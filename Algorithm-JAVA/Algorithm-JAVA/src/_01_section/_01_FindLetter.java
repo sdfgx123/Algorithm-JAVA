fe049e2ef6bd92c72623a146c90a990837482294
sdfgx123 <sdfgx123@naver.com>
2023-02-15 21:38:38 +0900

    20230215

--- Algorithm-JAVA/Algorithm-JAVA/src/_01_section/_01_FindLetter.java
+++ Algorithm-JAVA/Algorithm-JAVA/src/_01_section/_01_FindLetter.java
@@ -2,23 +2,29 @@ package _01_section;
 
 import java.util.Scanner;
 
-// 1번. 문자 찾기 : 문자 입력시 알파벳을 같이 입력하면 그 알파벳이 문장 안에 몇개나 포함되어 있는지 계산하는 알고리즘
+/**
+ * 1번 문자 찾기 : 문자 입력시 알파벳 같이 입력 > 그 알파벳이 문장 앞에 몇개나 포함돼 있는지 계산
+ */
 public class _01_FindLetter {
 
     public int solution(String str, char key) {
         int res = 0;
+        // str, key 전부 대문자로 변환
         str = str.toUpperCase();
         key = Character.toUpperCase(key);
-        // toCharArray : String 변수를 문자 하나하나 배열로 바꿔주는 메서드
+        // toCharArray : String 변수를 문자 하나하나 char 타입의 배열로 바꿔주는 메서드
         for (char i : str.toCharArray()) {
+            // 만약 char i가 char key와 같다면 > res에 count
             if (i == key) res++;
         }
         return res;
     }
 
     public static void main(String[] args) {
+
         _01_FindLetter main = new _01_FindLetter();
         Scanner sc = new Scanner(System.in);
+
         String str = sc.next();
         char key = sc.next().charAt(0);
         System.out.println(main.solution(str, key));
