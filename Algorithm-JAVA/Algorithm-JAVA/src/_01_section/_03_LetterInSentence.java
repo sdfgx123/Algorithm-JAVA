40feff00c87e8d137a977c0f1f91122649717fb5
sdfgx123 <sdfgx123@naver.com>
2023-02-18 15:21:25 +0900

    20230218

--- Algorithm-JAVA/Algorithm-JAVA/src/_01_section/_03_LetterInSentence.java
+++ Algorithm-JAVA/Algorithm-JAVA/src/_01_section/_03_LetterInSentence.java
@@ -23,11 +23,13 @@ public class _03_LetterInSentence {
     // 풀이 2번 : indexOf 활용해서 풀이
     public String solution_02(String str) {
         String res = "";
+        // min 변수를 최솟값으로 초기화
         int min = Integer.MIN_VALUE, pos;
         /**
          * pos 변수를, 공백이 등장했을 때의 인덱스로 초기화
          * -1이 나오는 경우는 공백이 없는 경우임
          * tmp 변수에, str을 0부터 pos 인덱스까지 잘라서 초기화 / 이때, 범위는 0 포함 0부터 pos 전까지임. pos index 포함 아님
+         * indexOf : 특정 문자나 문자열이 앞에서부터 처음 발견되는 인덱스를 반환, 만약 찾지 못할 경우 -1 반환
          */
         while ((pos = str.indexOf(" ")) != -1) {
             String tmp = str.substring(0, pos);
@@ -45,6 +47,7 @@ public class _03_LetterInSentence {
     public static void main(String[] args) {
         _03_LetterInSentence main = new _03_LetterInSentence();
         Scanner sc = new Scanner(System.in);
+        // nextLine() : 공백까지 포함해서 한 줄 전체를 다 받음
         String str = sc.nextLine();
         System.out.println(main.solution_02(str));
 
