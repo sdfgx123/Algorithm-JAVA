498e704d8a59fbef1b10c13e63179171f26a4555
sdfgx123 <sdfgx123@naver.com>
2023-02-25 22:09:43 +0900

    20230225

--- Algorithm-JAVA/Algorithm-JAVA/src/_02_section/_06_.java
+++ Algorithm-JAVA/Algorithm-JAVA/src/_02_section/_06_.java
@@ -13,7 +13,7 @@ public class _06_ {
 
     // 이 함수에서 뒤집은 수가 소수인지 판별
     public boolean isPrime(int num) {
-        // 넘어온 숫자가 1 > 소수가 아님 > return false
+        // 넘어온 숫자가 1 이라면 > 소수가 아님 > return false
         if (num==1) return false;
         // 2부터 num 바로 전 숫자까지 나눠 떨어지는 수가 있다면 > 소수 아님 > false
         for (int i=2; i<num; i++) {
@@ -30,6 +30,7 @@ public class _06_ {
         ArrayList<Integer> ans = new ArrayList<>();
         for (int i=0; i<n; i++) {
             int tmp = arr[i];
+            // res : 뒤집기 세팅 끝난 숫자 > isPrime(int num)에 넘겨서 소수인지 확인하는 작업 거치게 됨
             int res = 0;
             while (tmp>0) {
                 int t = tmp % 10;
