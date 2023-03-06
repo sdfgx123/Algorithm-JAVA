96194b063c209a1010a1a0e2f4456bfd2e54ea55
sdfgx123 <sdfgx123@naver.com>
2023-03-06 20:27:54 +0900

    20230306

--- Algorithm-JAVA/Algorithm-JAVA/src/_03_section/_02_.java
+++ Algorithm-JAVA/Algorithm-JAVA/src/_03_section/_02_.java
@@ -1,28 +1,32 @@
 package _03_section;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 두 배열 합치기
- * 같은 숫자가 나올 경우의 정렬은 고려하지 않음 > 같은 숫자가 나온 경우를 커버하는 게 생각나지 않음
+ * 공통원소 구하기
  */
-public class _01_ {
+public class _02_ {
 
     public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
         int p1 = 0, p2 = 0;
         while (p1<n && p2<m) {
-            if (a[p1]<b[p2]) answer.add(a[p1++]);
-            else answer.add(b[p2++]);
+            if (a[p1]==b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            }
+            else if (a[p1]<b[p2]) p1++;
+            else p2++;
         }
-        while (p1<n) answer.add(a[p1++]);
-        while (p2<m) answer.add(b[p2++]);
         return answer;
     }
 
     public static void main(String[] args) {
-        _01_ main = new _01_();
+        _02_ main = new _02_();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
