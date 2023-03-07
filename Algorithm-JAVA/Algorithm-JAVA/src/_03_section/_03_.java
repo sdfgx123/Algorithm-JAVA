235698499be8dcce215eecdbf8fcae80ab0bed0e
sdfgx123 <sdfgx123@naver.com>
2023-03-07 18:46:35 +0900

    20230307

---
+++ Algorithm-JAVA/Algorithm-JAVA/src/_03_section/_03_.java
@@ -0,0 +1,42 @@
+package _03_section;
+
+import java.util.Scanner;
+
+/**
+ * 3번 최대매출
+ */
+public class _03_ {
+
+//    public int solution(int n, int k, int[] arr) {
+//        int answer = 0, tmp = 0, max = Integer.MIN_VALUE;
+//        for (int i=0; i<n-3; i++) {
+//            tmp = arr[i] + arr[i+1] + arr[i+2];
+//            System.out.println("tmp : " + tmp + " max : " + max);
+//            if (tmp>max) max = tmp;
+//        }
+//        return max;
+//    }
+
+    public int solution(int n, int k, int[] arr) {
+        int answer, sum = 0;
+        for (int i=0; i<k; i++) sum += arr[i];
+        answer = sum;
+        for (int i=k; i<n; i++) {
+            sum += (arr[i]-arr[i-k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _03_ main = new _03_();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(main.solution(n, k, arr));
+    }
+}
---
+++ Algorithm-JAVA/Algorithm-JAVA/src/_03_section/_04_.java
@@ -0,0 +1,15 @@
+package _03_section;
+
+import java.util.Scanner;
+
+public class _04_ {
+
+    public int solution() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        _04_ main = new _04_();
+        Scanner sc = new Scanner(System.in);
+    }
+}
