3a81ec8c04f97ee25ee876df52680cfc0e879e1b
sdfgx123 <sdfgx123@naver.com>
2023-04-02 18:31:02 +0900

    20230402 commit
    5-3 크레인 카카오 기출문제

--- src/_05_section/_03_.java
+++ src/_05_section/_03_.java
@@ -1,11 +1,48 @@
 package _05_section;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  * 크레인 인형뽑기(카카오)
  */
 public class _03_ {
 
-    public static void main(String[] args) {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int pos : moves) {
+            for (int i=0; i<board.length; i++) {
+                if (board[i][pos-1]!=0) {
+                    int tmp = board[i][pos-1];
+                    board[i][pos-1] = 0;
+                    if (!stack.isEmpty() && tmp==stack.peek()) {
+                        answer += 2;
+                        stack.pop();
+                    }
+                    else stack.push(tmp);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
 
+    public static void main(String[] args) {
+        _03_ main = new _03_();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i=0; i<m; i++) {
+            moves[i] = sc.nextInt();
+        }
+        System.out.println(main.solution(board, moves));
     }
 }
