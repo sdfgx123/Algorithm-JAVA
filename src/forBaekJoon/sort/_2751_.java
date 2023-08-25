a81c51666df647858d7c85a5c11318f181bb31ec
sdfgx123 <sdfgx123@naver.com>
2023-08-25 21:11:18 +0900

    2751. 수 정렬하기 2

---
+++ src/forBaekJoon/sort/_2751_.java
@@ -0,0 +1,33 @@
+package forBaekJoon.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 2751. 수 정렬하기 2
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ */
+public class _2751_ {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i=0; i<n; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
